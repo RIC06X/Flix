6b1e60569251d47025d368b3e04ffd0f9e77ebc2
Yanjie Xu <yanjiexu06@gmail.com>
2020-04-15 00:28:38 +0800

    Initial Commit

---
+++ Flix Part 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix Part 1
+//
+//  Created by Yanjie Xu on 2020/4/15.
+//  Copyright © 2020 Yanjie Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
